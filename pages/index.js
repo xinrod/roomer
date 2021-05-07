5cbeaf7c30898e9059b1cf9d67313ba95eb56d1d
Roderick Liu <xinrod@live.unc.edu>
2021-05-07 02:50:08 -0400

    fix firestore issue

--- pages/index.js
+++ pages/index.js
@@ -11,31 +11,35 @@ import { getProfiles } from "../utils/firebase/db";
 
 function Home() {
   const auth = useAuth();
-  const [profiles, setProfiles] = useState();
-  
-  useEffect(()=>{
-    const fetchProfiles = async () => {
-      const profiles = await getProfiles();
-      setProfiles(profiles);
-    }
-    fetchProfiles();
-  },[])
-  return (
-    auth.user ? 
-    <Flex w="100vw" height="100vh" direction="column" align="center">
-      <Header w="100%" mb={5}/>
-      <Heading alignSelf="start" ml={6}>
-        People Looking for Roommates!
-      </Heading>
-      <Box overflow="scroll" w="100%" p={6}>
-        <Profiles profiles={profiles} />
-
-      </Box>
-    </Flex>
-    :
-    <Signin/>
+  if (auth.user) {
     
-  );
+    const [profiles, setProfiles] = useState();
+
+    useEffect(() => {
+      const fetchProfiles = async () => {
+        const profiles = await getProfiles();
+        setProfiles(profiles);
+      }
+      fetchProfiles();
+    }, [])
+    return (
+
+      <Flex w="100vw" height="100vh" direction="column" align="center">
+        <Header w="100%" mb={5} />
+        <Heading alignSelf="start" ml={6}>
+          People Looking for Roommates!
+      </Heading>
+        <Box overflow="scroll" w="100%" p={6}>
+          <Profiles profiles={profiles} />
+
+        </Box>
+      </Flex>
+
+
+
+    );
+  }
+  return (<Signin />)
 }
 
 export default Home;
