21271604aa1e272770d3dcde2420b659e00ca4da
Roderick Liu <xinrod@live.unc.edu>
2021-05-08 03:21:47 -0400

    fix campus

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -14,72 +14,88 @@ import { useRouter } from 'next/router';
 import { CUIAutoComplete } from 'chakra-ui-autocomplete'
 import { majors } from '../../utils/majors';
 
-function MajorAutocomplete({selectedItems, setSelectedItems}) {
-    let majorsFormatted = majors.map(val => ({value:val.value, label:val.value}));
+function MajorAutocomplete({ selectedItems, setSelectedItems }) {
+    let majorsFormatted = majors.map(val => ({ value: val.value, label: val.value }));
     const [pickerItems, setPickerItems] = useState(majorsFormatted);
-  
+
     const handleCreateItem = (item) => {
-      setPickerItems((curr) => [...curr, item]);
-      setSelectedItems((curr) => [...curr, item]);
+        setPickerItems((curr) => [...curr, item]);
+        setSelectedItems((curr) => [...curr, item]);
     };
-  
+
     const handleSelectedItemsChange = (selectedItems) => {
-      if (selectedItems) {
-        setSelectedItems(selectedItems);
-      }
+        if (selectedItems) {
+            setSelectedItems(selectedItems);
+        }
     };
-  
+
     return (
-      <FormControl id="major">
-          <CUIAutoComplete
-            label="Enter your Majors"
-            placeholder="Type a major"
-            onCreateItem={handleCreateItem}
-            items={pickerItems}
-            selectedItems={selectedItems}
-            onSelectedItemsChange={(changes) =>
-              handleSelectedItemsChange(changes.selectedItems)
-            }
-          />
-          </FormControl>
+        <FormControl id="major">
+            <CUIAutoComplete
+                label="Enter your Majors"
+                placeholder="Type a major"
+                onCreateItem={handleCreateItem}
+                items={pickerItems}
+                selectedItems={selectedItems}
+                onSelectedItemsChange={(changes) =>
+                    handleSelectedItemsChange(changes.selectedItems)
+                }
+            />
+        </FormControl>
     );
-  }
+}
 
 export default function Profile() {
     const auth = useAuth();
-    if (auth.user) {
-        const router = useRouter();
-        const [profileData, setProfileData] = useState({});
-        const [old, setOld] = useState({})
-        const [selectedItems, setSelectedItems] = useState([]);
-        useEffect(() => {
+
+    const router = useRouter();
+    const [profileData, setProfileData] = useState({});
+    const [old, setOld] = useState({})
+    const [selectedItems, setSelectedItems] = useState([]);
+    const [campus, setCampus] = useState("")
+    useEffect(() => {
+        if (auth.user) {
             const getOld = async (uid) => {
                 const res = await getProfile(uid);
                 setOld(res);
             }
             getOld(auth.user.uid)
-        }, [])
-        useEffect(async () => {
-            // console.log(profileData)
-            if (profileData != {} && 'major' in profileData && 'campus' in profileData && 'hometown' in profileData && 'bio' in profileData) {
-                await createProfile(auth.user.uid, profileData)
-                router.push('/')
-            }
-
-        }, [profileData])
-        const handleSubmit = (e) => {
-            e.preventDefault();
-            const majors = selectedItems.map((majorObject) => (majorObject.value)).reduce((acc, major)=>(acc + ", " + major), "").slice(2);
-
-            setProfileData({
-                name: auth.user.name,
-                major: majors,
-                campus: e.target.campus.value,
-                hometown: e.target.hometown.value,
-                grad: e.target.grad.value,
-                bio: e.target.bio.value,
-            })
         }
+    }, [auth])
+    useEffect(() => {
+        let majorArray = []
+        let campus = ""
+        if ("major" in old) {
+            majorArray = old.major.split(', ').map((val) => ({ value: val, label: val }))
+        }
+        setSelectedItems(majorArray)
+        if ("campus" in old) campus = old.campus
+        setCampus(campus);
+    }, [old])
+    useEffect(async () => {
+        if (profileData != {} && 'major' in profileData && 'campus' in profileData && 'hometown' in profileData && 'bio' in profileData) {
+            await createProfile(auth.user.uid, profileData)
+            router.push('/')
+        }
+
+    }, [profileData])
+    const handleCampusChange = (e) => {
+        setCampus(e);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const majors = selectedItems.map((majorObject) => (majorObject.value)).reduce((acc, major) => (acc + ", " + major), "").slice(2);
+
+        setProfileData({
+            name: e.target.name.value,
+            major: majors,
+            campus: e.target.campus.value,
+            hometown: e.target.hometown.value,
+            grad: e.target.grad.value,
+            bio: e.target.bio.value,
+        })
+    }
+    if (auth.user) {
         return (
 
             <Flex w="100vw" height="100vh" direction="column" align="center">
@@ -89,7 +105,15 @@ export default function Profile() {
                         Update your profile!
           </Heading>
                     <chakra.form onSubmit={handleSubmit}>
-                        <MajorAutocomplete selectedItems={selectedItems} setSelectedItems={setSelectedItems}/>
+                        <FormControl id="name">
+                            <FormLabel id="name">
+                                Enter your name
+                </FormLabel>
+                            <Input name="grad" defaultValue={old?.name} placeholder="Enter your name year">
+
+                            </Input>
+                        </FormControl>
+                        <MajorAutocomplete selectedItems={selectedItems} setSelectedItems={setSelectedItems} />
                         <FormControl id="grad">
                             <FormLabel id="grad">
                                 Graduation Date
@@ -100,10 +124,10 @@ export default function Profile() {
                         </FormControl>
                         <FormControl as="fieldset">
                             <FormLabel as="legend">Living on or off campus?</FormLabel>
-                            <RadioGroup defaultValue={old?.campus} name="campus">
+                            <RadioGroup value={campus} name="campus" colorScheme="blue" onChange={handleCampusChange}>
                                 <HStack>
-                                    <Radio value="on">on</Radio>
-                                    <Radio value="off">off</Radio>
+                                    <Radio value="on" border="1px">on</Radio>
+                                    <Radio value="off" border="1px">off</Radio>
                                 </HStack>
                             </RadioGroup>
                         </FormControl>
@@ -119,15 +143,15 @@ export default function Profile() {
 
                             </Textarea>
                         </FormControl>
-                        
+
                         <Box w="100%" align="start">
                             <Button type="submit">Update and post your profile!</Button>
                         </Box>
                         <Text colorScheme="telegram" fontSize="xs" my={4}>
                             Note: your profile picture is taken off of your 3rd party login, so if you want to change it, change it there! Sorry, file storage is expensive :(.
                         </Text>
-                        
-                    
+
+
                     </chakra.form>
                 </Flex>
             </Flex>
